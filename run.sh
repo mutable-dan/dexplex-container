d160eecdd9853c343f0d7d4b6e935a1d6de3eb94
672E2064616E <50145067+mutable-dan@users.noreply.github.com>
2024-08-07 19:22:11 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --rm --name dexplex -p 8080:8080 -it -v /home/gdan/dev/docker/dexplex/dexplex/logs:/dexplex/logs -v /home/gdan/dev/docker/dexplex/dexplex/config:/dexplex/config dexplex $1 $2 $3
+docker run --rm --name dexplex -p 8080:8080 -it -v ${PATH}/docker/dexplex/dexplex/logs:/dexplex/logs -v ${PATH}/docker/dexplex/dexplex/config:/dexplex/config dexplex $1 $2 $3
